e2a965d89acd5751e69548008856041d3794b251
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-24 16:39:37 +0500

    get paramas

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def main(request: Request) -> Response:
+    params = request.query_params
+
+    print(params['name'])
+
+    return Response({
+        "name": params['name']
+    })
